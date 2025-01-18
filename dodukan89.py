b7503931cbaebb2ab7f23364c9fc87fab37fb352
Dodukan89 <dodukan89@gmail.com>
2025-01-18 22:20:56 +0300

    dodukan89 v1

---
+++ dodukan89.py
@@ -0,0 +1,11 @@
+meme_dict = {
+            "cringe": "Garip ya da utandırıcı bir şey",
+            "lol": "Komik bir şeye verilen cevap",
+            "meme": "İrternette bir topluluk arasında sadece resim,video,cümle şeklinde manasını herkesin anlayamayacağı bir paylaşım çeşidi"
+            }
+word = input("Anlamadığınız bir kelime yazın (hepsini küçük harfle yazın!): ")
+if word in meme_dict.keys():
+    print(meme_dict[word])
+    print("Bunların anlamı budur")
+else:
+    print("sözlükte buna karşılık bişey yok tekrar dene")
